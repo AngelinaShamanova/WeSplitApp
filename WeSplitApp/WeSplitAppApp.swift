e8b95f2bf35ee4b24ec3d472d5249b65ba683da8
AngelinaShamanova <shamanovaaaa@yandex.ru>
2022-11-13 20:34:21 +0100

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Ангелина Шаманова on 13.11.22..
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
